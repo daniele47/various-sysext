bf9b2cf8d83a802da27794d539ba8c508e062b46
Private <private@invalid.local>
2026-03-28 14:04:30 +0000

    split install into build/install since they need to happen in different env (one inside a container, one on the host)

--- broadcom-wl-dkms/build.sh
+++ broadcom-wl-dkms/build.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/bash
 
+### THIS SCRIPT NEEDS TO BE RUN INSIDE AN ARCHLINUX CONTAINER ###
+
 set -e
 
+KERNEL_VERSION=$(uname -r)
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 BUILD_DIR="$SCRIPT_DIR/.build"
-SYSEXT_DIR="$SCRIPT_DIR/.sysext"
-KERNEL_VERSION=$(uname -r)
-EXT_NAME="broadcom-wl-dkms"
-EXT_BASE_DIR="/var/lib/extensions"
-EXT_DIR="$EXT_BASE_DIR/$EXT_NAME"
-LOCAL_EXT_DIR="$SYSEXT_DIR"
-
-### BUILD SYSEXT DIRECTORY ###
+ALL_SYSEXT_DIR="$SCRIPT_DIR/.sysext"
+SYSEXT_DIR="$ALL_SYSEXT_DIR/$KERNEL_VERSION/broadcom-wl-dkms"
 
 # install deps
 sudo pacman -Syu --noconfirm --needed base-devel make                                   # make/gcc/... to build
 sudo pacman -Syu --noconfirm --needed linux-headers linux-zen-headers linux-lts-headers # convers most linux headers required to build
 
 # cleanup build dir if already exists
-rm -rf "$BUILD_DIR" "$SYSEXT_DIR"
+rm -rf "$BUILD_DIR" "$ALL_SYSEXT_DIR"
 
 # copy all into build dir
 mkdir -p "$BUILD_DIR" "$SYSEXT_DIR"
@@ -34,20 +31,3 @@ mkdir -p "$SYSEXT_DIR/usr/lib/modules/$KERNEL_VERSION/extra"
 cp "$BUILD_DIR/broadcom-wl-dkms/usr/src/broadcom-wl-6.30.223.271/wl.ko" "$SYSEXT_DIR/usr/lib/modules/$KERNEL_VERSION/extra"
 cp -r "$BUILD_DIR/broadcom-wl-dkms/usr/lib/modprobe.d" "$SYSEXT_DIR/usr/lib/"
 cp -r "$BUILD_DIR/broadcom-wl-dkms/usr/share" "$SYSEXT_DIR/usr/share"
-
-### INSTALL OR UPDATE SYSTEM EXTENSION ###
-
-updated=false
-if [[ -e "$EXT_DIR" ]]; then
-    updated=true
-    sudo rm -rf "$EXT_DIR"
-fi
-
-sudo mkdir -p "$EXT_BASE_DIR"
-sudo cp -r "$LOCAL_EXT_DIR" "$EXT_BASE_DIR"
-
-if "$updated"; then
-    echo "Extension updated! Merge system extensions or reboot to make it available!"
-else
-    echo "Extension installed! Merge system extensions or reboot to make it available!"
-fi
