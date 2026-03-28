a2f5942b80b48bc25e6fbbb3661d4c8255ecfcad
Private <private@invalid.local>
2026-03-28 13:21:22 +0000

    copy into build dir

--- broadcom-wl-dkms/build_in_archlinux_container.sh
+++ broadcom-wl-dkms/build_in_archlinux_container.sh
@@ -8,8 +8,13 @@ sudo pacman -Syu --noconfirm --needed linux-headers linux-zen-headers linux-lts-
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-BUILD_DIR="$SCRIPT_DIR/build"
+BUILD_DIR="$SCRIPT_DIR/.build"
+BRCM_DIR="$SCRIPT_DIR/broadcom-wl-dkms"
 
 # cleanup build dir if already exists
 rm -rf "$BUILD_DIR"
+
+# copy all into build dir
+mkdir -p "$BUILD_DIR"
+cp -r "$BRCM_DIR" "$BUILD_DIR"
 
