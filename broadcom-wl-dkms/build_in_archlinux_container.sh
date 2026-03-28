869dd61d0a52353a7d7d36baeabc2d609b69c0ce
Private <private@invalid.local>
2026-03-28 13:17:16 +0000

    adding build script

---
+++ broadcom-wl-dkms/build_in_archlinux_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# install deps
+sudo pacman -Syu --noconfirm --needed base-devel make # make/gcc/... to build
+sudo pacman -Syu --noconfirm --needed linux-headers linux-zen-headers linux-lts-headers # convers most linux headers required to build
+
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+BUILD_DIR="$SCRIPT_DIR/build"
+
+# cleanup build dir if already exists
+rm -rf "$BUILD_DIR"
+
