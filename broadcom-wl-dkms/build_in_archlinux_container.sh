22d3f3ad33f5c4c25f8d17b2aa007cf11280937a
Private <private@invalid.local>
2026-03-28 13:24:29 +0000

    added build step

--- broadcom-wl-dkms/build_in_archlinux_container.sh
+++ broadcom-wl-dkms/build_in_archlinux_container.sh
@@ -9,12 +9,14 @@ sudo pacman -Syu --noconfirm --needed linux-headers linux-zen-headers linux-lts-
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 BUILD_DIR="$SCRIPT_DIR/.build"
-BRCM_DIR="$SCRIPT_DIR/broadcom-wl-dkms"
 
 # cleanup build dir if already exists
 rm -rf "$BUILD_DIR"
 
 # copy all into build dir
 mkdir -p "$BUILD_DIR"
-cp -r "$BRCM_DIR" "$BUILD_DIR"
+cp -r "$SCRIPT_DIR/broadcom-wl-dkms" "$BUILD_DIR"
 
+# move into correct dir
+! cd "$BUILD_DIR/broadcom-wl-dkms/usr/src/broadcom-wl-6.30.223.271" && echo 'failed to enter correct dir' && exit 1
+make -C "/lib/modules/$(uname -r)/build" "M=$(pwd)" modules
